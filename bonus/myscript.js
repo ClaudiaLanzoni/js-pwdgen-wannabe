eb2d6a7a6c041ea271777d9c0d1ec5a74d03f147
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-08-04 18:26:39 +0200

    bonus finalized

--- bonus/myscript.js
+++ bonus/myscript.js
@@ -1,29 +1,36 @@
 
-var dataDiOggi = new Date("08/04/2021");
+var dataDiOggi = new Date();
+
+console.log(dataDiOggi);
+
+//FORNISCE FORMATO DATA GG/MM/AA PERò NON RENDE POSSIBILE LA SOTTRAZIONE
+
 //var giorno = String(dataDiOggi.getDate()).padStart(2, '0');
 //var mese = String(dataDiOggi.getMonth() + 1).padStart(2, '0');
 //var anno = dataDiOggi.getFullYear();
 //dataDiOggi = giorno + '/' + mese + '/' + anno;
-
-document.getElementById("data-corrente").innerHTML = dataDiOggi;
+//document.getElementById("data-corrente").innerHTML = dataDiOggi;
 
 var mioCompleanno = new Date("11/28/2021");
+
+console.log(mioCompleanno);
+
+//FORNISCE FORMATO DATA GG/MM/AA PERò NON RENDE POSSIBILE LA SOTTRAZIONE
+
 //var giorno = String(mioCompleanno.getDate()).padStart(2, '0');
+//var mese = String(mioCompleanno.getMonth() + 1).padStart(2, '0');
 //var anno = mioCompleanno.getFullYear();
 //mioCompleanno = giorno + '/' + mese + '/' + anno;
-
-document.getElementById("mio-compleanno").innerHTML = mioCompleanno;
-
-var differenza = mioCompleanno.getTime() - dataDiOggi.getTime();
+//document.getElementById("mio-compleanno").innerHTML = mioCompleanno;
 
 
-var giorniDifferenza = differenza / (1000 * 3600 * 24);
+//CALCOLO DIFFERENZA DI GIORNI TRA LA DATA DEL MIO COMPLEANNO E OGGI
+var giorniDifferenza = (Math.floor((mioCompleanno - dataDiOggi) / (1000*60*60*24)));
 
-giorniDifferenza = parseInt(giorniDifferenza);
-
-document.getElementById("differenza-giorni").innerHTML = giorniDifferenza;
+console.log(giorniDifferenza);
 
 
+//ALERT DOPO AVER SCHIACCIATO BOTTONE
 function calcolatoreCompleanno() {
    alert (giorniDifferenza);
   }
