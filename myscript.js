979e2e64e017796d9d947d23d3ae0d4c3d3a0571
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-08-04 15:11:12 +0200

    finalized exercise

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var Nome = prompt("Inserisci il tuo nome");
+
+var Cognome = prompt("Inserisci il tuo cognome");
+
+var ColorePreferito = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("password").innerHTML = Nome + Cognome + ColorePreferito + "21";
+
